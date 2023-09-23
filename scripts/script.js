059d494141b7dc413fd5d148302df970a87e1bde
Multyp <miguel.dasilva2004@hotmail.com>
2023-09-23 21:14:17 +0200

    wip

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,9 @@
 import ThemeSwitcher from './theme.js';
 import Library from './library.js';
+import Tutorial from './tutorial.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   new ThemeSwitcher();
   new Library();
-
-  const openInstructionsBtn = document.getElementById('openInstructionsBtn');
-    const closeInstructionsBtn = document.getElementById('closeInstructionsBtn');
-    const instructionsModal = document.getElementById('instructionsModal');
-    
-    const prevSlideBtn = document.getElementById('prevSlideBtn');
-    const nextSlideBtn = document.getElementById('nextSlideBtn');
-    const carousel = document.querySelector('.carousel');
-    let currentSlide = 0;
-
-    openInstructionsBtn.addEventListener('click', () => {
-      instructionsModal.classList.remove('hidden');
-      showSlide(currentSlide);
-    });
-
-    closeInstructionsBtn.addEventListener('click', () => {
-      instructionsModal.classList.add('hidden');
-    });
-
-    function showSlide(slideIndex) {
-      const slides = document.querySelectorAll('.carousel-item');
-      slides.forEach((slide, index) => {
-        if (index === slideIndex) {
-          slide.style.display = 'flex';
-        } else {
-          slide.style.display = 'none';
-        }
-      });
-    }
-  
-    function updateHowTo() {
-      const body = document.body;
-      const image1 = document.getElementById("HowTo-1");
-      const image2 = document.getElementById("HowTo-2");
-    
-      if (body.classList.contains("light-theme")) {
-        image1.src = "assets/light-add-a-book.png";
-        image2.src = "assets/light-add-form.png";
-      } else {
-        image1.src = "assets/dark-add-a-book.png";
-        image2.src = "assets/dark-add-form.png";
-      }
-    }
-
-    const observer = new MutationObserver(updateHowTo);
-    observer.observe(document.body, { attributes: true, attributeFilter: ["class"] });
-
-    updateHowTo();
-
-    prevSlideBtn.addEventListener('click', () => {
-      currentSlide = Math.max(currentSlide - 1, 0);
-      showSlide(currentSlide);
-    });
-
-    nextSlideBtn.addEventListener('click', () => {
-      currentSlide = Math.min(currentSlide + 1, carousel.childElementCount - 1);
-      showSlide(currentSlide);
-    });
+  new Tutorial();
 });
---
+++ scripts/tutorial.js
@@ -0,0 +1,68 @@
+class Tutorial {
+  constructor() {
+    this.openInstructionsBtn = document.getElementById('openInstructionsBtn');
+    this.closeInstructionsBtn = document.getElementById('closeInstructionsBtn');
+    this.instructionsModal = document.getElementById('instructionsModal');
+    this.prevSlideBtn = document.getElementById('prevSlideBtn');
+    this.nextSlideBtn = document.getElementById('nextSlideBtn');
+    this.carousel = document.querySelector('.carousel');
+    this.currentSlide = 0;
+
+    this.openInstructionsBtn.addEventListener('click', () => {
+      this.instructionsModal.classList.remove('hidden');
+      this.showSlide(this.currentSlide);
+    });
+
+    this.closeInstructionsBtn.addEventListener('click', () => {
+      this.instructionsModal.classList.add('hidden');
+    });
+
+    this.prevSlideBtn.addEventListener('click', () => {
+      this.currentSlide = Math.max(this.currentSlide - 1, 0);
+      this.showSlide(this.currentSlide);
+    });
+
+    this.nextSlideBtn.addEventListener('click', () => {
+      this.currentSlide = Math.min(this.currentSlide + 1, this.carousel.childElementCount - 1);
+      this.showSlide(this.currentSlide);
+    });
+
+    this.updateHowTo();
+    this.observeThemeChanges();
+  }
+
+  showSlide(slideIndex) {
+    const slides = document.querySelectorAll('.carousel-item');
+    slides.forEach((slide, index) => {
+      if (index === slideIndex) {
+        slide.style.display = 'flex';
+      } else {
+        slide.style.display = 'none';
+      }
+    });
+  }
+
+  updateHowTo() {
+    const body = document.body;
+    const image1 = document.getElementById("HowTo-1");
+    const image2 = document.getElementById("HowTo-2");
+    const image3 = document.getElementById("HowTo-3");
+
+    if (body.classList.contains("light-theme")) {
+      image1.src = "assets/light-add-a-book.png";
+      image2.src = "assets/light-add-form.png";
+      image3.src = "assets/light-book.png";
+    } else {
+      image1.src = "assets/dark-add-a-book.png";
+      image2.src = "assets/dark-add-form.png";
+      image3.src = "assets/dark-book.png";
+    }
+  }
+
+  observeThemeChanges() {
+    const observer = new MutationObserver(this.updateHowTo);
+    observer.observe(document.body, { attributes: true, attributeFilter: ["class"] });
+  }
+}
+
+export default Tutorial;
